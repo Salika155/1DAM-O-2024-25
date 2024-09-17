9dee352c909336fb3a6746fe3d08e6825b046c2a
Salika155 <62901117+Salika155@users.noreply.github.com>
2024-09-17 20:26:16 +0200

    vfdfs
    d

---
+++ prueba/ConsoleApp1/ConsoleApp1/Funciones.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    internal class Funciones
+    {
+        public static int GetGreather3(int a, int b, int c)
+        {
+            int result = a;
+            if (b > result)
+            {
+                result = b;
+            }
+            if (c > result)
+            {
+                result = c;
+            }
+            return result;
+        }
+
+        
+    }
+}

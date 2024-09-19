f8b21eb7aa31010b3f2f46a100e567b02e7cf80d
Salika155 <62901117+Salika155@users.noreply.github.com>
2024-09-19 16:38:09 +0200

    f

--- Programacion/Ejercicios/18-09-24/Ejercicio1/ConsoleApp1/Ejercicios.cs
+++ Programacion/Ejercicios/18-09-24/Ejercicio1/ConsoleApp1/Ejercicios.cs
@@ -27,6 +27,9 @@ namespace ConsoleApp1
         //Funcion que me de una de las dos soluciones de la ecuacion de segundo grado
         public static double SolveSecondGradeEquation(double a, double b, double c)
         {
+            if (a == 0 || b == 0 || c == 0)
+                return double.NaN;
+
             double discriminante = (b * b) - 4 * (a * c);
             double sqrt = Math.Sqrt(discriminante) / 2 * a;
 
@@ -37,9 +40,28 @@ namespace ConsoleApp1
             {
                 return double.NaN;
             }
-            return result2;
+            return result1;
             //a = 1, b = -4, c = 3;
             // - b +- (raiz(b^2 - 4 + a + c)/ 2 * a)
+
+        }
+
+        public static double SolveSecondGradeEquation1(double a, double b, double c)
+        {
+            if (a == 0)
+                return double.NaN;
+
+            double paso1 = 2.0 * a;
+            double paso2 = (b * b);
+            double paso3 = 4.0 * a * c;
+            double paso4 = paso2 - paso3;
+            if (paso4 < 0)
+                return double.NaN;
+
+            double paso5 = Math.Sqrt(paso4);
+            double paso6 = -b + paso5;
+            double paso7 = paso6 / paso1;
+            return paso7;
 
         }
     }

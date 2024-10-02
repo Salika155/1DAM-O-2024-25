05995bc7a997525aa50050feac642c333914ed11
Salika155 <carre155@hotmail.com>
2024-10-02 12:32:32 +0200

    s

---
+++ Programacion/Ejercicios/EjerciciosPDF/EjerciciosPDF/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace EjerciciosPDF
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}

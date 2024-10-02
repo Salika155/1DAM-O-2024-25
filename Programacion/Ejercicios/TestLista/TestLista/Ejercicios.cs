b6c024e30aa05187796f6df761a6595b7fd92e81
Salika155 <62901117+Salika155@users.noreply.github.com>
2024-10-02 16:50:55 +0200

    s

---
+++ Programacion/Ejercicios/TestLista/TestLista/Ejercicios.cs
@@ -0,0 +1,92 @@
+﻿
+
+namespace test_list
+{
+    internal class Ejercicios
+    {
+        //Hacer una funcion que le paso una lista de enteros y los imprime por pantalla
+
+        public static void PrintNumbers(List<int> l) 
+        {
+            if (l == null)
+            Console.WriteLine("La lista tiene " + l.Count + " elementos.");
+
+            for (int i = 0; i < l.Count; i++)
+            {
+                var element = l[i];
+                Console.WriteLine(element);
+            }
+        }
+
+        //Hacer una funcion que le paso una lista de enteros y me devuelve la suma de todos esos numeros
+
+        public static int SumaElementosLista(List<int> l)
+        {
+            if (l == null)
+                return 0;
+            int result = 0;
+            
+            for (int i = 0; i < l.Count; i++)
+            {
+                //int element = l[i];
+                //result += element;
+                result += l[i];
+            }
+            return result;
+        }
+
+        //Funcion que le paso una lista de enteros y devuelva la suma de todos sus numeros pares
+
+        public static int SumaElementosParesLista(List<int> l)
+        {
+            if (l == null)
+                return 0;
+            int result = 0;
+
+            for (int i = 0; i < l.Count; i++)
+            {
+                int element = l[i];
+                if (element % 2 == 0) //mejor usar la funcion
+                    result += element;
+            }
+            return result;
+        }
+
+        //Hacer una funcion que le paso una lista de enteros y me devuelve una lista que sea una copia exacta que la lista que le he pasado
+
+        public static List<int> CopiarLista(List<int> l)
+        {
+            List<int> l1 = new List<int>();
+            if (l == null)
+                return l;
+            
+            for (int i = 0; i < l.Count; i++)
+            {
+                int element = l[i];
+                l1.Add(element);
+            }
+            return l1;
+        }
+
+        //Hacer una funcion que le paso una lista de enteros y devuelve una lista al reves
+
+        public static List<int> CopiarListaInversa(List<int> l)
+        {
+            List<int> lcopia = new();
+
+            for(int i = l.Count - 1; i >= 0; i--)
+            {
+                int element = l[i];
+                lcopia.Add(element);
+                //result.Insert(0, element);
+
+                //int index = l.Count - i - 1;
+                //int element = l[index];
+                //result.Add(element);
+            }
+            return lcopia;
+        }
+
+        //Hacer una funcion que le paso una lista de enteros (no devuelve nada) y los invierte
+    }
+}
---
+++ Programacion/Ejercicios/TestLista/TestLista/Program.cs
@@ -0,0 +1,37 @@
+﻿namespace test_list
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            List<int> list = new();
+            List<int> list1 = new List<int>();
+
+            list.Add(7);
+            list.Add(-1);
+            list.Add(0);
+            list.Add(3);
+
+            int n = list.Count();
+            list.Add(5);
+            n = list.Count();
+
+            Ejercicios.PrintNumbers(list);
+
+            int suma = Ejercicios.SumaElementosLista(list);
+            Console.WriteLine(suma);
+
+            int sumaPar = Ejercicios.SumaElementosParesLista(list);
+            Console.WriteLine(sumaPar);
+
+            List<int> l2 = Ejercicios.CopiarLista(list);
+
+            Ejercicios.CopiarLista(list);
+
+            Ejercicios.CopiarListaInversa(list);
+            //Remove en lista es suspenso, se usa RemoveAt
+
+
+        }
+    }
+}

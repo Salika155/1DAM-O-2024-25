a74428b362be41508ee368576f22e69e1abd5103
Salika155 <carre155@hotmail.com>
2024-10-13 03:59:47 +0200

    sd

--- Programacion/otras cosas/Trucos/POO/Game.cs
+++ Programacion/otras cosas/Trucos/POO/Game.cs
@@ -6,8 +6,34 @@ using System.Threading.Tasks;
 
 namespace POO
 {
+     
     public class Game
     {
-        Game salikaGame = new Game();
+        private decimal _money;
+        public string Name { get; set; }
+        public decimal Prize
+        {
+            get { return _money; }
+            set
+            {
+                if (value < 0)
+                {
+                    value = 0;
+                }
+                _money = value;
+            }
+        }
+
+        public Game(string name, decimal prize)
+        {
+            Name = name;
+            Prize = prize;
+        }
+
+        public string GetInfo()
+        {
+            return "Nombre: " + Name + ", Precio: $ " + Prize; 
+        }
+
     }
 }

bb43195f0bb0f92ca3ee2ea07ab80dd9d5349973
Salika155 <62901117+Salika155@users.noreply.github.com>
2024-10-17 19:01:29 +0200

    ds

---
+++ Programacion/Ejercicios/PokerGame/PokerGame/Card.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PokerGame
+{
+    internal class Card
+    {
+        public int Number;
+        public CardType Kind;
+        public ColorType Color;
+        
+        public Card()
+        {
+
+        }
+
+        public Card(int number, ColorType color , CardType kind)
+        {
+            this.Number = number;
+            this.Color = color;
+            this.Kind = kind;
+        }
+
+        public void SetNumber(int number)
+        {
+            this.Number = number;
+        }
+    }
+}
---
+++ Programacion/Ejercicios/PokerGame/PokerGame/Program.cs
@@ -0,0 +1,22 @@
+﻿namespace PokerGame
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Card c1 = new Card();
+            c1.Number = 1;
+            c1.SetNumber(3);
+
+            Card c2 = new Card()
+            {
+                Number = 5,
+                Color = ColorType.BLACK,
+                Kind = CardType.CLOVER
+            };
+            c2.SetNumber(10);
+
+            Card c3 = new Card(10, ColorType.BLACK, CardType.CLOVER);
+        }
+    }
+}

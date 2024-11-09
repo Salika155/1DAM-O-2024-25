95b0d03e7ef22ac6ff514f491d35bbb7afd87f2e
Salika155 <carre155@hotmail.com>
2024-11-09 04:05:00 +0100

    as

---
+++ Programacion/SheepAndWolfs/SheepAndWolfs/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace SheepAndWolfs
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}

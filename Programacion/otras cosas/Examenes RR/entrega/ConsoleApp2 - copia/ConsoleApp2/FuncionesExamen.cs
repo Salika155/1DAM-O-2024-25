c436e3b0d9370fa1b243e3f62d2b1ae1a263512f
Salika155 <carre155@hotmail.com>
2024-11-27 12:15:05 +0100

    hy

---
+++ Programacion/otras cosas/Examenes RR/entrega/ConsoleApp2 - copia/ConsoleApp2/FuncionesExamen.cs
@@ -0,0 +1,90 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp2
+{
+    public class FuncionesExamen
+    {
+        public static bool Infernal(int n)
+        {
+            if (n == 0)
+                return false;
+
+            string num = n.ToString();
+            int aux = 6;
+            int count = 0;
+
+            if (num.Length == 0)
+                return false;
+
+            for(int i = 0, j = i + 1; i < num.Length; i++, j++)
+            {
+                for(int k = j + 1; k < num.Length; k++)
+                {
+                    char iaux = num[i];
+                    char jaux = num[j];
+                    char kaux = num[k];
+                    if (iaux == aux)
+                    {
+                        if (jaux == aux)
+                        {
+                            if (kaux == aux)
+                            {
+                                return true;
+                            }
+                        }
+                    }
+                }
+            }
+            return false;
+           
+        }
+
+        //public static int IndexOf
+
+        public static bool AreEquals(char a, char b)
+        {
+            return (a == b) ? true : false;
+        }
+
+        //EJ 2
+        public static int SumaNumerosPrimos(int n)
+        {
+            if (n <= 0)
+                return 0;
+
+            int aux = 0;
+            int count = 0;
+
+            while (count <= n)
+            {
+                for(int i = 0; i < n; i++)
+                {
+                    if (IsPrimo(i))
+                    {
+                        aux += i;
+                        count++;
+                    }
+                }
+            }
+            return aux;
+
+        }
+
+        public static bool IsPrimo(int n)
+        {
+            if (n <= 1)
+                return false;
+
+            for (int i = 0; i <= n; i ++)
+            {
+                if (i % n == 0 || i % 2 != 0)
+                    return true;
+            }
+            return false;
+        }
+    }
+}
---
+++ Programacion/otras cosas/Examenes RR/entrega2/ExamenListaArray - copia/ExamenListaArray/Program.cs
@@ -0,0 +1,44 @@
+﻿namespace ExamenListaArray
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+            //EX1
+
+            int[] arrayNumeros = new int[] { 3, 1, 5, 4, 2};
+            int[] array2 = new int[] { 40, 10, 50, 20, 30 };
+
+
+            double numeromedio = Funciones.GetNumNearMidValor(arrayNumeros);
+            Console.Write(numeromedio);
+
+            double numeromedio2 = Funciones.GetNumNearMidValor(array2);
+            Console.Write(numeromedio2);
+
+
+            //EX2
+            List<string> listaCompra = new List<string>();
+            
+            string s = "manzana";
+            listaCompra.Add(s);
+            string s2 = "banana";
+            listaCompra.Add(s2);
+            string s3 = "cereza";
+            listaCompra.Add(s3);
+            string s4 = "durazno";
+            listaCompra.Add(s4);
+
+            List<string> listaFiltro = new List<string>();
+
+            listaFiltro.Add(s2);
+            listaFiltro.Add(s4);
+
+            Funciones.RemoveRepeatedValors(listaCompra, listaFiltro);
+            Console.Write(listaCompra);
+
+
+        }
+    }
+}

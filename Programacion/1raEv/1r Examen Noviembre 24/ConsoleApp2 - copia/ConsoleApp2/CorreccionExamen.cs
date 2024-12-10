019559e6bdfe12510363ad4678366de4c8833683
Salika155 <carre155@hotmail.com>
2024-12-10 15:00:24 +0100

    da

--- Programacion/1raEv/1r Examen Noviembre 24/ConsoleApp2 - copia/ConsoleApp2/CorreccionExamen.cs
+++ Programacion/1raEv/1r Examen Noviembre 24/ConsoleApp2 - copia/ConsoleApp2/CorreccionExamen.cs
@@ -8,7 +8,71 @@ namespace ConsoleApp2
 {
     internal class CorreccionExamen
     {
-        
+        public static bool IsInfernal(int n)
+        {
+            #region comentado
+            if (n == 0)
+                return false;
+
+            n = Math.Abs(n);
+            int count6numbers = 0;
+
+            while (n > 0)
+            {
+                if (n % 10 == 6)
+                {
+                    count6numbers++;
+                    if (count6numbers == 3)
+                        return true;
+                }
+                else
+                    count6numbers = 0;
+                n /= 10;
+            }
+            return false;
+            #endregion
+
+        }
+
+        public static int SumaNumerosPrimos(int n)
+        {
+            #region comentado
+            if (n <= 0)
+                return 0;
+
+            int suma = 0;
+            int count = 0;
+            int numPrimo = 2;
+
+            while (count < n)
+            {
+                if (IsPrime(numPrimo))
+                {
+                    suma += numPrimo;
+                    count++;
+                }
+                numPrimo++;
+            }
+            return suma;
+            #endregion
+
+        }
+
+        public static bool IsPrime(int n)
+        {
+            #region comentado
+            if (n <= 1)
+                return false;
+
+            for (int i = 2; i < n; i++)
+            {
+                if (n % i == 0)
+                    return false;
+            }
+            return true;
+            #endregion
+
+        }
     }
 }
 
--- Programacion/1raEv/1r Examen Noviembre 24/ConsoleApp2 - copia/ConsoleApp2/Program.cs
+++ Programacion/1raEv/1r Examen Noviembre 24/ConsoleApp2 - copia/ConsoleApp2/Program.cs
@@ -1,25 +1,49 @@
-﻿namespace ConsoleApp2
+﻿using System.Threading.Channels;
+
+namespace ConsoleApp2
 {
     internal class Program
     {
         static void Main(string[] args)
         {
 
-            int numeroInfernal = 666;
-            bool isNumeroInfernal = FuncionesExamen.Infernal(numeroInfernal);
-            Console.WriteLine(isNumeroInfernal);
-            int numeroInfernal2 = 4447666;
-            bool isNumeroInfernal2 = FuncionesExamen.Infernal(numeroInfernal);
-            Console.WriteLine(isNumeroInfernal);
-            int numeroInfernal3 = 74643;
-            bool isNumeroInfernal3 = FuncionesExamen.Infernal(numeroInfernal);
-            Console.WriteLine(isNumeroInfernal);
+            //int numeroInfernal = 666;
+            //bool isNumeroInfernal = FuncionesExamen.Infernal(numeroInfernal);
+            //Console.WriteLine(isNumeroInfernal);
+            //int numeroInfernal2 = 4447666;
+            //bool isNumeroInfernal2 = FuncionesExamen.Infernal(numeroInfernal);
+            //Console.WriteLine(isNumeroInfernal);
+            //int numeroInfernal3 = 74643;
+            //bool isNumeroInfernal3 = FuncionesExamen.Infernal(numeroInfernal);
 
+            //int primo = 5;
+            //bool esprimo = FuncionesExamen.IsPrimo(primo);
+            //int seriePrimos = FuncionesExamen.SumaNumerosPrimos(primo);
+            //Console.WriteLine(seriePrimos);
 
-            int primo = 5;
-            bool esprimo = FuncionesExamen.IsPrimo(primo);
-            int seriePrimos = FuncionesExamen.SumaNumerosPrimos(primo);
-            Console.WriteLine(seriePrimos);
+            //Clase Correcccion
+
+            Console.WriteLine("Ejercicio 1");
+            int numeroInfernalC = 666;
+            bool isNumeroInfernalC = CorreccionExamen.IsInfernal(numeroInfernalC);
+            Console.WriteLine(isNumeroInfernalC);
+            int numeroInfernal2C = 4447666;
+            bool isNumeroInfernal2C = CorreccionExamen.IsInfernal(numeroInfernal2C);
+            Console.WriteLine(isNumeroInfernal2C);
+            int numeroInfernal3C = 74643;
+            bool isNumeroInfernal3C = CorreccionExamen.IsInfernal(numeroInfernal3C);
+            Console.WriteLine(isNumeroInfernal3C);
+            int numeroInfernal4C = 7466643;
+            bool isNumeroInfernal4C = CorreccionExamen.IsInfernal(numeroInfernal4C);
+            Console.WriteLine(isNumeroInfernal4C);
+
+            Console.WriteLine("Ejercicio 2");
+            int primoC = 5;
+            bool esprimoC = CorreccionExamen.IsPrime(primoC);
+            int sumaPrimosC = CorreccionExamen.SumaNumerosPrimos(primoC);
+            //Console.WriteLine(sumaPrimosC);
+            Console.WriteLine($"La suma de los primeros {primoC} números primos es: {sumaPrimosC}");
+
         }
     }
 }

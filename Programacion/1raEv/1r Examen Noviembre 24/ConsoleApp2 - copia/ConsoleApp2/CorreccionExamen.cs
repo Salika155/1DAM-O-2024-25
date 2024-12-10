4a9b4e72b89e371edbb5a9e5743357b95c7bf887
Salika155 <carre155@hotmail.com>
2024-12-10 15:14:27 +0100

    ujg

--- Programacion/1raEv/1r Examen Noviembre 24/ConsoleApp2 - copia/ConsoleApp2/CorreccionExamen.cs
+++ Programacion/1raEv/1r Examen Noviembre 24/ConsoleApp2 - copia/ConsoleApp2/CorreccionExamen.cs
@@ -11,67 +11,109 @@ namespace ConsoleApp2
         public static bool IsInfernal(int n)
         {
             #region comentado
+            //if (n == 0)
+            //    return false;
+
+            //n = Math.Abs(n);
+            //int count6numbers = 0;
+
+            //while (n > 0)
+            //{
+            //    if (n % 10 == 6)
+            //    {
+            //        count6numbers++;
+            //        if (count6numbers == 3)
+            //            return true;
+            //    }
+            //    else
+            //        count6numbers = 0;
+            //    n /= 10;
+            //}
+            //return false;
+            #endregion
             if (n == 0)
                 return false;
 
+            int countNumInf = 0;
             n = Math.Abs(n);
-            int count6numbers = 0;
 
             while (n > 0)
             {
                 if (n % 10 == 6)
                 {
-                    count6numbers++;
-                    if (count6numbers == 3)
+                    countNumInf++;
+                    if (countNumInf == 3)
                         return true;
                 }
                 else
-                    count6numbers = 0;
-                n /= 10;
+                    countNumInf = 0;
+                n /= 10; 
             }
             return false;
-            #endregion
-
         }
 
         public static int SumaNumerosPrimos(int n)
         {
             #region comentado
+            //if (n <= 0)
+            //    return 0;
+
+            //int suma = 0;
+            //int count = 0;
+            //int numPrimo = 2;
+
+            //while (count < n)
+            //{
+            //    if (IsPrime(numPrimo))
+            //    {
+            //        suma += numPrimo;
+            //        count++;
+            //    }
+            //    numPrimo++;
+            //}
+            //return suma;
+            #endregion
             if (n <= 0)
                 return 0;
 
-            int suma = 0;
-            int count = 0;
-            int numPrimo = 2;
+            int sumaNum = 0;
+            int countNum = 0;
+            int PrimoNum = 2;
 
-            while (count < n)
+            while (countNum < n)
             {
-                if (IsPrime(numPrimo))
+                if (IsPrime(PrimoNum))
                 {
-                    suma += numPrimo;
-                    count++;
+                    sumaNum += PrimoNum;
+                    countNum++;
                 }
-                numPrimo++;
+                PrimoNum++;
             }
-            return suma;
-            #endregion
-
+            return sumaNum;
         }
 
         public static bool IsPrime(int n)
         {
             #region comentado
+            //if (n <= 1)
+            //    return false;
+
+            //for (int i = 2; i < n; i++)
+            //{
+            //    if (n % i == 0)
+            //        return false;
+            //}
+            //return true;
+            #endregion
             if (n <= 1)
                 return false;
 
             for (int i = 2; i < n; i++)
             {
-                if (n % i == 0)
+                if (n % 2 == 0)
                     return false;
             }
             return true;
-            #endregion
-
         }
     }
 }

d21042780a481e069fbeb9e3e05e4b7575abd20e
Salika155 <carre155@hotmail.com>
2024-12-19 20:52:22 +0100

    kjh

---
+++ Programacion/2daEv/AlgoritmoSwapNLetrasDePalabra/AlgoritmoSwapNLetrasDePalabra/ArrayAlgoritmo.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AlgoritmoSwapNLetrasDePalabra
+{
+    internal class ArrayAlgoritmo
+    {
+        //cambiar la posicion de dos letras seguidas en una palabra, o mas
+
+    }
+}

73c37a03b72b4397aa497f4761d98e47828e9694
Salika155 <62901117+Salika155@users.noreply.github.com>
2024-12-27 00:57:18 +0100

    n

--- Programacion/2daEv/AlgoritmoSwapNLetrasDePalabra/AlgoritmoSwapNLetrasDePalabra/ArrayAlgoritmo.cs
+++ Programacion/2daEv/AlgoritmoSwapNLetrasDePalabra/AlgoritmoSwapNLetrasDePalabra/ArrayAlgoritmo.cs
@@ -11,10 +11,68 @@ namespace AlgoritmoSwapNLetrasDePalabra
         //cambiar la posicion de dos letras seguidas en una palabra, o mas
         //sin hacer un nuevo array
 
-        public static void E1(int[] array, int n)
+        public static void E1(string[] array, int n)
         {
             if (array == null || n <= 0)
                 return;
+
+            int lenght = array.Length;
+
+            for(int i = 0; i < lenght - (lenght - i); i++)
+            {
+                if (array.Length % 2 == 0)
+                {
+                    string aux1 = array[lenght - (lenght - i)];
+                    string auxarray0 = array[0 + lenght - i];
+                    string swapvar = aux1;
+                    aux1 = auxarray0;
+                    auxarray0 = swapvar;
+                    swapvar = aux1;
+                }
+                else
+                {
+
+                }
+            }
+        }
+    }
+
+    public static void E2(string[] array, int n)
+    {
+        int inicio = 0;
+        int fin = array.Length - 1;
+
+        while(inicio < fin)
+        {
+            string aux = array[inicio];
+            array[inicio] = array[fin];
+            array[fin] = aux;
+            inicio++;
+            fin--;
+        }
+
+        //aqui rotarias todo el array
+        //ahora falta rotar sof otra vez e ia otra vez
+        inicio = 0;
+        fin = n - 1;
+        while (inicio < fin)
+        {
+            string aux = array[inicio];
+            array[inicio] = array[fin];
+            array[fin] = aux;
+            inicio++;
+            fin--;
+        }
+        // con esto se resuelve
+        inicio = n;
+        fin = array.Length - 1;
+        while(inicio < fin)
+        {
+            string aux = array[inicio];
+            array[inicio] = array[fin];
+            array[fin] = aux;
+            inicio++;
+            fin--;
         }
     }
 }
--- Programacion/2daEv/AlgoritmoSwapNLetrasDePalabra/AlgoritmoSwapNLetrasDePalabra/Program.cs
+++ Programacion/2daEv/AlgoritmoSwapNLetrasDePalabra/AlgoritmoSwapNLetrasDePalabra/Program.cs
@@ -4,7 +4,19 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            string[] palabra = ["H","O","L","A"];
+            ArrayAlgoritmo.E1(palabra, 2);
+
+            string[] palabra2 = ["S", "O", "F", "I", "A"];
+            ArrayAlgoritmo.E1(palabra2, 3);
+
+            string[] palabra3 = ["H", "O", "L", "A"];
+            ArrayAlgoritmo.E2(palabra, 2);
+
+            string[] palabra4 = ["S", "O", "F", "I", "A"];
+            ArrayAlgoritmo.E2(palabra2, 3);
+
+
         }
     }
 }

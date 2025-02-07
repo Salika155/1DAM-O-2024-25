4fb33a9419a03ba3cde580e42a8b5eaf3e8ef20b
Salika155 <carre155@hotmail.com>
2025-02-07 14:59:25 +0100

    uh

---
+++ Programacion/Ajedrez/ChessLib/ChessLib/GameController.cs
@@ -0,0 +1,18 @@
+﻿using ChessLib.Figuras;
+using ChessLib.Tablero;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ChessLib
+{
+    public class GameController
+    {
+        private ChessBoard _tablero;
+        private FigureColor _turnoActual;
+        private Coord? _piezaSeleccionada;
+        private List<Coord> _movimientosValidos;
+    }
+}

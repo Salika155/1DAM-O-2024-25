df0363fb77cd0919a1a3d4190f50599b80f65dcd
Salika155 <carre155@hotmail.com>
2025-03-01 21:36:17 +0100

    ij

--- Programacion/2daEv/DragonBallEx/DragonBallEx/Torneo.cs
+++ Programacion/2daEv/DragonBallEx/DragonBallEx/Torneo.cs
@@ -11,28 +11,39 @@ namespace DragonBallEx
         public delegate void VisitAction(Persona participante);
 
         private readonly List<Persona>? _participantes;
+        private static int _participantCounter = 1;
+
 
         public Torneo()
         {
             _participantes = new List<Persona>();
         }
-           
+
         public void Init()
         {
             if (_participantes == null)
                 return;
 
-            for (int i = 0; i <= 16; i++)
+            for (int i = 0; i < 16; i++)
             {
-                _participantes.Add(CreateParticipantes(i));
-                
-
+                _participantes.Add(CreateParticipante());
             }
         }
 
-        private Persona CreateParticipantes(int index)
+
+        //public static void CreateParticipantes(int numParticipantes)
+        //{
+        //    for (int i = 0; i <= numParticipantes; i++)
+        //    {
+        //        CreateParticipante();
+        //    }
+        //}
+
+        //mejor un createParticipante y el Participantes puede ser practicamente el init no?
+
+        public static Persona CreateParticipante()
         {
-            string n = string.Empty;
+            string n = $"Persona{_participantCounter++}";
             double randomType = Utils.GetRandom(0, 1);
             if (randomType < 0.25)
             {
@@ -52,7 +63,47 @@ namespace DragonBallEx
 
         public List<string> Execute()
         {
-            throw new NotImplementedException();
+            if (_participantes == null || _participantes.Count != 16)
+                throw new InvalidOperationException("Torneo Invalido");
+
+            List<string> resultados = new List<string>();
+            List<Persona> participantesActivos = new List<Persona>(_participantes);
+
+            while(participantesActivos.Count > 1)
+            {
+                List<Persona> ganadoresRonda = new List<Persona>();
+                for(int i = 0; i < ganadoresRonda.Count; i += 2)
+                {
+                    Persona participante1 = participantesActivos[i];
+                    Persona participante2 = participantesActivos[i + 1];
+
+                    string? resultadoCombate = SimularCombate(participante1, participante2);
+                    if (resultadoCombate == null)
+                        return resultados;
+                    resultados.Add(resultadoCombate);
+
+                    Persona ganador = participante1.Energia > 0 ? participante1 : participante2;
+                    ganadoresRonda.Add(ganador);
+                }
+                participantesActivos = ganadoresRonda;
+
+            }
+            resultados.Add($"{participantesActivos[0].Name} es el ganador del torneo!");
+            return resultados;
+        }
+
+        private string? SimularCombate(Persona participante1, Persona participante2)
+        {
+            while(participante1.Energia > 0 && participante2.Energia > 0)
+            {
+                participante1.Atacar(participante2);
+                if (participante2.Energia > 0)
+                {
+                    participante2.Atacar(participante1);
+                }
+            }
+            string? nombreGanador = participante1.Energia > 0 ? participante1.Name : participante2.Name;
+            return nombreGanador;
         }
 
         public void Visit(VisitAction action)
@@ -65,6 +116,7 @@ namespace DragonBallEx
                 action(participante);
             }
         }
+
         
     }
 }

5ac9f2e79662280368742dee6b6dab80d549630c
Salika155 <62901117+Salika155@users.noreply.github.com>
2025-03-04 19:30:51 +0100

    lk

---
+++ Programacion/2daEv/PracticaExamen/PracticaExamen/Program.cs
@@ -0,0 +1,22 @@
+﻿namespace PracticaExamen
+{
+    internal class Program
+    {
+        
+        public static bool AreEquals<T>(T item1, T item2) where T : IComparable<T> 
+        {
+            if (item1.CompareTo(item2) == 0)
+                return false;
+            if (item1 is null)
+                return false;
+
+            if (item1.Equals(item2))
+                return true;
+
+            if (EqualityComparer<T>.Default.Equals(item1, item2))
+                return true;
+            return false;
+        }
+        
+    }
+}
---
+++ Programacion/2daEv/PracticaExamen/PracticaExamen/Student.cs
@@ -0,0 +1,74 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PracticaExamen
+{
+    class Student : IComparable<Student>
+    {
+        public string Name = string.Empty;
+        public int Id = 0;
+        public static bool operator == (Student? a, Student? b)
+        {
+            if (a is null)
+                return false;
+            return true;
+        }
+
+        public static bool operator != (Student? a, Student? b)
+        {
+            return true;
+        }
+
+        public int CompareTo(Student? other)
+        {
+            throw new NotImplementedException();
+        }
+
+        public override bool Equals(object? obj)
+        {
+            if (obj == null)
+                return false;
+            if (GetType() == obj.GetType())
+            {
+                Student s = (Student)obj;
+                return s.Name == Name;
+            }
+            return base.Equals(obj);
+        }
+
+        //selainventa
+        public bool Match(Comparison<Student> comparer, Student other)
+        {
+            if (comparer == null)
+                return false;
+            return comparer(this, other) == 0;
+        }
+        public int CompareTo(Student? other)
+        {
+            if (other == null)
+                return -1;
+            int n;
+            n = Name.CompareTo(other.Name);
+            if (n != 0)
+                return n;
+            //n = Id.CompareTo(other.Id); -> un boxing haciendo un new por debajo -> coger esa variable de stack y pasarla a heap
+            if (Id > other.Id)
+                return -1;
+            if (Id < other.Id)
+                return 1;
+
+            return 0;
+        }
+        //sagrado para diccionarios o cualquier coleccion que quiera hacer uso de tu hash
+        public override int GetHashCode()
+        {
+            //para el caso de que el name sea el key
+            return Name.GetHashCode() + Id;
+        }
+    }
+}
+
+//servidor de ajedreces

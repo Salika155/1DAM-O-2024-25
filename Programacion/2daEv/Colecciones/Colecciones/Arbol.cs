8d9fa0e5ff71fa3fed05e156233d4ff0f2f59723
Salika155 <carre155@hotmail.com>
2025-03-05 18:38:05 +0100

    th

--- Programacion/2daEv/Colecciones/Colecciones/Arbol.cs
+++ Programacion/2daEv/Colecciones/Colecciones/Arbol.cs
@@ -8,5 +8,65 @@ namespace Colecciones
 {
     class Arbol<T> : IArbol<T>
     {
+        public Nodo<T> Root { get; set; }
+        private List<T> _nodoList = new();
+
+        public Arbol(Nodo<T> root)
+        {
+            Root = root;
+        }
+
+        public void AddChild(Nodo<T> hijo)
+        {
+            if (hijo == null)
+                return;
+            if (_nodoList[i] == hijo)
+            _nodoList.Add(hijo);
+        }
+
+        public int ChildCount()
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Clear()
+        {
+            throw new NotImplementedException();
+        }
+
+        public bool Contains(Nodo<T> nodo)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Detatch()
+        {
+            throw new NotImplementedException();
+        }
+
+        public List<Nodo<T>> GetAncestors()
+        {
+            throw new NotImplementedException();
+        }
+
+        public Nodo<T> GetChildAt(int index)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Nodo<T> GetRoot()
+        {
+            throw new NotImplementedException();
+        }
+
+        public bool IsRoot(Nodo<T> nodo)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void RemoveChild(Nodo<T> hijo)
+        {
+            throw new NotImplementedException();
+        }
     }
 }
--- Programacion/2daEv/Colecciones/Colecciones/IArbol.cs
+++ Programacion/2daEv/Colecciones/Colecciones/IArbol.cs
@@ -8,5 +8,35 @@ namespace Colecciones
 {
     interface IArbol<T>
     {
+        #region codigo
+        //Nodo<T> GetRoot();
+        //void AddChild();
+        //void RemoveChild(Nodo<T> hijo);
+        //Nodo<T>? GetChildAt(int index); 
+        //int ChildCount();
+        //void Clear();
+        //bool Contains(Nodo<T> nodo);
+        //List<Nodo<T>> GetAncestors();
+        //bool IsRoot();
+        //void Detatch();
+        //Nodo<T> Parent { get; set; }
+        //T item { get; set; }
+        #endregion
+
+        T item { get; set; }
+        IArbol<T> Parent { get; set; }
+        int ChildCount();
+
+
+        Nodo<T> GetRoot();
+        void AddChild(Nodo<T> hijo);
+        void Detatch();
+        bool IsRoot(Nodo<T> nodo);
+        Nodo<T> GetChildAt(int index);
+        bool Contains(Nodo<T> nodo);
+        void Clear();
+        
+        void RemoveChild(Nodo<T> hijo);
+        List<Nodo<T>> GetAncestors(); 
     }
 }

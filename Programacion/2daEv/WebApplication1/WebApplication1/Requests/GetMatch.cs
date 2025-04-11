4c5d0a5dd1a229a4c5c7ac6b901d641ea5eb56f6
Salika155 <carre155@hotmail.com>
2025-04-11 12:02:49 +0200

    gu

--- Programacion/2daEv/WebApplication1/WebApplication1/Requests/GetMatch.cs
+++ Programacion/2daEv/WebApplication1/WebApplication1/Requests/GetMatch.cs
@@ -6,21 +6,27 @@
         {
             public Figure[] Figures { get; internal set; }
 
-            internal class Figure
-            {
-                private BattleField.FigureType type;
-                private BattleField.FigureColor color;
-                private int x;
-                private int y;
+            //internal class Figure
+            //{
+            //    private BattleField.FigureType type;
+            //    private BattleField.FigureColor color;
+            //    private int x;
+            //    private int y;
 
-                public Figure(BattleField.FigureType type, BattleField.FigureColor color, int x, int y)
-                {
-                    this.type = type;
-                    this.color = color;
-                    this.x = x;
-                    this.y = y;
-                }
-            }
+            //    public Figure(BattleField.FigureType type, BattleField.FigureColor color, int x, int y)
+            //    {
+            //        this.type = type;
+            //        this.color = color;
+            //        this.x = x;
+            //        this.y = y;
+            //    }
+            //}
+            public record Figure(
+            BattleField.FigureType FigureType,
+            BattleField.FigureColor Color,
+            int x,
+            int y
+            );
         }
 
         //dentro tiene que tener un request y response

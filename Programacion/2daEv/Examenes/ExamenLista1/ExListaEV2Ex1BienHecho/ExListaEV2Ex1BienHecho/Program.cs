33d58b3c41f5b79c7c9e758922443ebdfe8e7d93
Salika155 <carre155@hotmail.com>
2025-04-14 21:26:28 +0200

    fh

---
+++ Programacion/2daEv/Examenes/ExamenLista1/ExListaEV2Ex1BienHecho/ExListaEV2Ex1BienHecho/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace ExListaEV2Ex1BienHecho
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}

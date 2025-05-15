14d76ac2942dcc3c05738e213cc15a907f6fdf86
Salika155 <carre155@hotmail.com>
2025-05-15 19:59:40 +0200

    bn

---
+++ Programacion/2daEv/Blueprint/ExamenBienHechoBlueprint/ExamenBienHechoBlueprint/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace ExamenBienHechoBlueprint
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}

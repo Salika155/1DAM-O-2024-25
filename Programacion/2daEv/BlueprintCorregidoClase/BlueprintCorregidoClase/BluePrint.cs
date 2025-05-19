942df0790c46835c317dc009e7179c51b10551b8
Salika155 <62901117+Salika155@users.noreply.github.com>
2025-05-19 23:49:58 +0200

    hui

---
+++ Programacion/2daEv/BlueprintCorregidoClase/BlueprintCorregidoClase/BluePrint.cs
@@ -0,0 +1,82 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BlueprintCorregidoClase
+{
+    public interface IBluePrint
+    {
+        public delegate void VisitDelegate(IShape shape);
+        public delegate bool FilterDelegate(IShape shape);
+        int GetShapeCount();
+        IShape GetShapeAt(int index);
+        void AddShape(IShape shape);
+        void RemoveShape(FilterDelegate del);
+        void RemoveShape(IShape shape);
+        IShape GetShape(VisitDelegate del);
+
+
+
+
+    }
+    public class BluePrint : IBluePrint
+    {
+        private readonly List<IShape> _shapes = new();
+        //ishape le he dado el permiso de tener un ishape -> funcion de abajo 
+        //ajedrez genera sus propias fichas, aqui no se puede
+        public void AddShape(IShape shape)
+        {
+            //Shape sp = (Shape)shape;
+            //_shapes.Add(sp);
+            if (!ContainsChild(shape))
+            {
+                _shapes.Add(shape);
+                shape.Owner = this;
+            }
+
+        }
+
+        private bool ContainsChild(IShape shape)
+        {
+            throw new NotImplementedException();
+        }
+
+        public IShape GetShape(IBluePrint.VisitDelegate del)
+        {
+            throw new NotImplementedException();
+        }
+
+        public IShape GetShapeAt(int index)
+        {
+            throw new NotImplementedException();
+        }
+
+        public int GetShapeCount()
+        {
+            throw new NotImplementedException();
+        }
+
+        public void RemoveShape(IBluePrint.FilterDelegate del)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void RemoveShape(IShape shape)
+        {
+            for (int i = 0; i < _shapes.Count; i++)
+            {
+                if (_shapes[i] == shape)
+                {
+                    shape.Owner = null;
+                    _shapes.RemoveAt(i);
+                    return;
+
+                }
+            }
+        }
+
+        //hacer casting bien mediante un if para comprobar que sea
+    }
+}
---
+++ Programacion/2daEv/BlueprintCorregidoClase/BlueprintCorregidoClase/Shape.cs
@@ -0,0 +1,99 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BlueprintCorregidoClase
+{
+    public interface IShape
+    {
+        string Name;
+
+        BluePrint Owner { get; set; }
+    }
+    class Shape : IShape
+    {
+        public string _name;
+        private readonly Color _color;
+        //la tengo que hacer debil
+        private WeakReference<IBluePrint>? _blueprint = new WeakReference<IBluePrint>();
+
+        
+
+        public IBluePrint? Owner
+        { get => GetOwner(); 
+            set => SetOwner(); }
+        public string Name { get => _name; set => _name = value; }
+        public Color => _color;
+        public abstract bool HasArea { get; }
+        public abstract double Area { get; }
+        public abstract double Perimeter { get; }
+        public abstract Point2D Point { 
+        }
+
+        private static IBluePrint GetOwner()
+        {
+            if (_blueprint == null)
+                return null;
+            if (_blueprint.TryGetTarget(out var result))
+                return result;
+            return null;
+        }
+
+        public void SetOwner(IBluePrint? newparent)
+        {
+            if (newparent == null)
+            {
+                //voy a conseguir a mi padre actual
+                var oldparent = GetOwner();
+
+                if (newparent== oldparent)
+                    return;
+
+                //mi nuevo padre es null, pero mi antiguo padre no
+                if (newparent == null)
+                {
+                    _blueprint == null;
+                    oldparent!.RemoveShape(this);
+                }
+                else
+                {
+                    //mi nuevo padre no es null
+                    if(oldparent != null)
+                    {
+                        oldparent.RemoveShape(this);
+                    }
+                    if (newparent != null)
+                    {
+                        oldparent.AddShape(this);
+                        _blueprint = new WeakReference<IBluePrint>();
+                    }
+                }
+
+                
+            }
+        }
+
+        public Shape(string name, Color color)
+        {
+            _name = name;
+            _color = color;
+        }
+
+        public void RemoveShape(IShape shape)
+        {
+            for( int i = 0; i < _shapes.Count; i++)
+            {
+                if (_shapes[i] == shape)
+                {
+                    shape.Owner = null;
+                    _shapes.RemoveAt(i);
+                    return;
+
+                }
+            }
+        }
+
+    }
+}

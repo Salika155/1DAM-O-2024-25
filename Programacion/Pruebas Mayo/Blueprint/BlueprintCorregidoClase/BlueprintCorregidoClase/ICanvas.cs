ddf6e26ccd5b473b1465177cd4f724fc0391e258
Salika155 <carre155@hotmail.com>
2025-05-20 23:05:34 +0200

    ilh

---
+++ Programacion/Pruebas Mayo/Blueprint/BlueprintCorregidoClase/BlueprintCorregidoClase/ICanvas.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BlueprintCorregidoClase
+{
+    interface ICanvas
+    {
+    }
+
+    public struct Point2D
+    {
+
+    }
+
+    public struct Vector2D
+    {
+
+    }
+
+    public struct Rect2D
+    {
+        public Point2D Position;
+        public double Width;
+        public double Height;
+
+        public Point2D MaxPosition => new Point2D(Position.X + Width, Position.Y + Height);
+
+        public Point2D MinPosition => Position;
+
+        public Rect2D(Point2D position, double width, double height);
+
+        public override string ToString()
+        {
+            return $"X: {Position.X} Y, {Position.Y}";
+
+        }
+    }
+
+    public struct Color
+    {
+        public double R;
+        public double G;
+        public double B;
+    }
+}
---
+++ Programacion/Pruebas Mayo/Blueprint/BlueprintCorregidoClase/BlueprintCorregidoClase/Program.cs
@@ -0,0 +1,15 @@
+﻿namespace BlueprintCorregidoClase
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            IBluePrint blueprint = new BluePrint();
+            IShape shape1 = new Hector();
+            IShap3 shape2 = new Circle("", new Color());
+
+            blueprint.AddShape(shape2);
+            blueprint.AddShape(shape1);
+        }
+    }
+}
